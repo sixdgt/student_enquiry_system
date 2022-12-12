09d96a5a29a4d1b8b997248b21397c529763e99e
sixdgt <c4crypt@gmail.com>
2022-12-12 16:52:12 +0545

    student create, edit & show page

--- project_ses/app_ses/urls.py
+++ project_ses/app_ses/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("students/", views.student_index, name="students.index"),
+    path("students/create/", views.student_create, name="students.create"),
+    path("students/edit/", views.student_edit, name="students.edit"),
+    path("students/show/", views.student_show, name="students.show"),
 ]
--- project_ses/app_ses/views.py
+++ project_ses/app_ses/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def student_index(request):
     return render(request, "students/index.html")
+
+def student_create(request):
+    return render(request, "students/create.html")
+
+def student_show(request):
+    return render(request, "students/show.html")
+
+def student_edit(request):
+    return render(request, "students/edit.html")
+    
