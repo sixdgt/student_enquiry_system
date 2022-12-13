43887af6a494577b04e97e9f2ca15568aeb564ae
sixdgt <c4crypt@gmail.com>
2022-12-13 16:53:05 +0545

    form rendered into template - student create

--- project_ses/app_ses/views.py
+++ project_ses/app_ses/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-
+from .forms import StudentCreateForm, CourseCreateForm, UserRegisterForm, UserLoginForm
 # Create your views here.
 def student_index(request):
     return render(request, "students/index.html")
 
 def student_create(request):
-    return render(request, "students/create.html")
+    std_create_form = StudentCreateForm()
+    context = {
+        "temp_form": std_create_form
+    }
+    return render(request, "students/create.html", context)
 
 def student_show(request):
     return render(request, "students/show.html")
