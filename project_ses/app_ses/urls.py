b120719b8f9697a2cb929d223890b4a419f9638a
sixdgt <c4crypt@gmail.com>
2022-12-16 17:04:17 +0545

    student update

--- project_ses/app_ses/urls.py
+++ project_ses/app_ses/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("students/", views.student_index, name="students.index"),
     path("students/create/", views.student_create, name="students.create"),
+    path("students/update/", views.student_update, name="students.update"),
     path("students/edit/<int:id>/", views.student_edit, name="students.edit"),
     path("students/show/<int:id>/", views.student_show, name="students.show"),
     path("students/delete/<int:id>/", views.student_delete, name="students.delete"),
--- project_ses/app_ses/views.py
+++ project_ses/app_ses/views.py
@@ -38,6 +38,29 @@ def student_create(request):
 
     return render(request, "students/create.html", context)
 
+def student_update(request):
+    if request.method == "POST":
+        course = Course.objects.get(id=request.POST.get("course_id"))
+        std_obj = Student.objects.get(id=request.POST.get('id'))
+        std_obj.first_name = request.POST.get("first_name")
+        std_obj.middle_name = request.POST.get("middle_name")
+        std_obj.last_name = request.POST.get("last_name")
+        std_obj.email = request.POST.get("email")
+        std_obj.contact = request.POST.get("contact")
+        std_obj.gender = request.POST.get("gender")
+        std_obj.blood_group = request.POST.get("blood_group")
+        std_obj.academic_level = request.POST.get("academic_level")
+        std_obj.academic_status = request.POST.get("academic_status")
+        std_obj.academic_org = request.POST.get("academic_org")
+        std_obj.academic_score = request.POST.get("academic_score")
+        std_obj.course = course
+        std_obj.intake = request.POST.get("intake")
+        std_obj.shift = request.POST.get("shift")
+        std_obj.remarks = request.POST.get("remarks")
+        std_obj.save()
+    
+    return redirect("students.index")
+
 def student_show(request, id):
     data = Student.objects.get(id=id)
     context = {"data": data}
@@ -45,7 +68,8 @@ def student_show(request, id):
 
 def student_edit(request, id):
     data = Student.objects.get(id=id)
-    context = {"data": data}
+    courses = Course.objects.all()
+    context = {"data": data, "courses": courses}
     return render(request, "students/edit.html", context)
 
 def student_delete(request, id):
